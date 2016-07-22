905bdcd12baaa27480e9b7210b3c6c3a66ad3ee7
Rishab Verma <rishabv16@gmail.com>
2016-07-22 00:50:59 -0400

    Update flex_counter.sv

--- Lab4/source/flex_counter.sv
+++ Lab4/source/flex_counter.sv
@@ -1,7 +1,7 @@
 // $ID: $
 // File name: $  
 // Created:     $
-// Author:      $
+// Author:      Rishab Verma$
 // Lab Section: $
 // Version:     $
 // Description: $
@@ -81,88 +81,3 @@ always_ff @ (posedge clk, negedge n_rst) begin
 
 
 endmodule
-
-
-/*
-`timescale 1ns /100ps
-
-module adder_1bit
-(
-	input wire a,
-	input wire b,
-	input wire carry_in,
-	
-	output reg sum,
-	output reg carry_out
-);
- 
-always @ (a,b, carry_in)
-   begin
-      assert(( a == 1'b1) || (a == 1'b0))
-      else $error("Input 'a' of component is not a digital logic value");
-
-      assert((b == 1'b1) || (b == 1'b0))
-      else $error("Input 'b' of component is not a digital logic value");
-
-      assert((carry_in == 1'b1) || (carry_in == 1'b0))
-      else $error("Input 'carry_in' of component is not a digital logic value");
-	
-   end
-
-
-
-  assign sum = carry_in ^ (a ^ b);
-  assign carry_out = ((! carry_in) & b & a) | (carry_in & (b | a));
-
-
-   
-   always @ (a, b, carry_in)
-   begin
-     #(2) assert (((a+b+carry_in)%2) == sum)
-       else $error("Output 's' of first 1 bit adder is not correct");
-   end
-	
-
-endmodule
-`timescale 1ns /100ps
-
-module adder_1bit
-(
-	input wire a,
-	input wire b,
-	input wire carry_in,
-	
-	output reg sum,
-	output reg carry_out
-);
- 
-always @ (a,b, carry_in)
-   begin
-      assert(( a == 1'b1) || (a == 1'b0))
-      else $error("Input 'a' of component is not a digital logic value");
-
-      assert((b == 1'b1) || (b == 1'b0))
-      else $error("Input 'b' of component is not a digital logic value");
-
-      assert((carry_in == 1'b1) || (carry_in == 1'b0))
-      else $error("Input 'carry_in' of component is not a digital logic value");
-	
-   end
-
-
-
-  assign sum = carry_in ^ (a ^ b);
-  assign carry_out = ((! carry_in) & b & a) | (carry_in & (b | a));
-
-
-   
-   always @ (a, b, carry_in)
-   begin
-     #(2) assert (((a+b+carry_in)%2) == sum)
-       else $error("Output 's' of first 1 bit adder is not correct");
-   end
-	
-
-endmodule
-
-*/
